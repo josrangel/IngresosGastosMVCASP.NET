ede7625981e5b173e483d4d72895da1c54453b98
Josue <josrangel@hotmail.com>
2021-04-13 17:50:26 -0500

    initial commit

---
+++ IngresosGastos/Data/AppDBContext.cs
@@ -0,0 +1,25 @@
+﻿using IngresosGastos.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace IngresosGastos.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext() : base("DefaultConnection") //del connectionStrings que agregamos en el webconfig, especificamos cual es el que utilizara
+        {
+                
+        }
+
+        public DbSet<IngresosGastosJRC> IngresosGastos { get; set; }
+
+        /**
+         * tutorial https://www.udemy.com/course/aprende-aspnet-mvc-framework-y-base-de-datos-ms-sql-server
+         Comandos para la consola del administrador de paquetes 
+        enable-migrations
+         */
+    }
+}
---
+++ IngresosGastos/Models/IngresosGastosJRC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IngresosGastos.Models
+{
+    public class IngresosGastosJRC
+    {
+        [Key]
+        private int Id { get; set; }
+
+        [Required]
+        [StringLength (60, MinimumLength = 3)]
+        private string Descripcion { get; set; }
+
+        [Required]
+        private bool EsIngreso { get; set; }
+        
+        [Required]
+        private double Valor { get; set; }
+
+    }
+}
